8672709ba1a53fb16443758c4b1a6f080363858c
Washington <was2023hington@gmail.com>
2025-08-24 17:42:23 -0300

    feat: tornando usuario serializavel para persistencia

--- Gerenciador de tarefas/src/Usuario.java
+++ Gerenciador de tarefas/src/Usuario.java
@@ -1,4 +1,8 @@
-public class Usuario {
+import java.io.Serializable;
+
+public class Usuario implements Serializable{
+    private static final long serialVersionUID = 1L;
+
     private int id;
     private String nome;
     private String email;
