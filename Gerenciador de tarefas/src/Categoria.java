08f891605d36ae338ba6b00274f3db6a711c406b
Washington <was2023hington@gmail.com>
2025-08-24 17:47:55 -0300

    tornando categoria serializavel

--- Gerenciador de tarefas/src/Categoria.java
+++ Gerenciador de tarefas/src/Categoria.java
@@ -1,4 +1,8 @@
-public class Categoria {
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+    private static final long serialVersionUID = 1L;
+
     private int id;
     private String nome;
     private String cor;
